df9742e0681a8e3ed83cb1ea8497f489e66831c5
David Saunders <david@monkee-boy.com>
2019-01-17 13:25:38 -0500

    Create createItem GraphQL mutation

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,3 +1,14 @@
-const Query = {};
+// Need to set up a resolver whenever you create a new query
+const Query = {
+  // Each request contains four variables
+  // parent - parent schema in graphql
+  // args - arguments that have been passed to the query
+  // ctx - the context - surface the database and provides the rest of the request (headers, cookies, etc.)
+  // info - provides information on the graphql query that's coming in
+  // dogs(parent, args, ctx, info) {
+  //   global.dogs = global.dogs || [];
+  //   return global.dogs;
+  // }
+};
 
 module.exports = Query;
